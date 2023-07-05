d1e808463369b3ed3cd97de4b5c5f923a0ea844b
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2023-07-05 03:05:19 -0500

    Spring Framework

--- src/main/java/com/example/learnspringframework/helloworld/App02HelloWorld.java
+++ src/main/java/com/example/learnspringframework/helloworld/App02HelloWorld.java
@@ -3,6 +3,8 @@ package com.example.learnspringframework.helloworld;
 import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorld {
     public static void main(String[] args) {
 
@@ -10,7 +12,12 @@ public class App02HelloWorld {
 			System.out.println(context.getBean("name"));
 			System.out.println(context.getBean("age"));
 			System.out.println(context.getBean("person"));
-			System.out.println(context.getBean("address"));
+			System.out.println(context.getBean("person2"));
+			System.out.println(context.getBean("person3"));
+			System.out.println(context.getBean(Address.class));
+			System.out.println(context.getBean(Person.class));
+			System.out.println();
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 		} catch (BeansException e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/example/learnspringframework/helloworld/HelloWorldConfiguration.java
+++ src/main/java/com/example/learnspringframework/helloworld/HelloWorldConfiguration.java
@@ -2,13 +2,13 @@ package com.example.learnspringframework.helloworld;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 
-record Person(String name, int age) { }
+record Person(String name, int age, Address address) { }
 record Address(String firstLine, String city) { }
 @Configuration
 public class HelloWorldConfiguration {
-
     @Bean
     public String name(){
         return "Sunny Sumanth Dodda";
@@ -17,12 +17,36 @@ public class HelloWorldConfiguration {
     public int age(){
         return 25;
     }
-    @Bean
-    public Person person(){
-        return new Person("John Wick",40);
-    }
+
     @Bean
     public Address address(){
-        return new Address("123 Alma Rd","VA");
+        return new Address("123 Sigma Rd","TX");
     }
+
+    @Bean
+    public Address address2(){
+        return new Address("123 MacArthur Dr","TX");
+    }
+
+    @Bean
+    @Primary
+    public Address address3(){
+        return new Address("Baker St","London");
+    }
+    @Bean
+    public Person person(){
+        return new Person(name(),age(),address());
+    }
+
+    @Bean
+    @Primary
+    public Person person2(Address address2){
+        return new Person("John Wick",40,address2);
+    }
+
+    @Bean
+    public Person person3(Address address3){
+        return new Person("Sherlock Holmes",40,address3);
+    }
+
 }
