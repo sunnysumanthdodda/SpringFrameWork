bbc2f048df93a44756fc3a3c679f1fc457142c0a
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2024-08-13 00:44:35 -0500

    Alien springframework

--- src/main/java/com/springframework/learnspringframework2/GamingAppLaunchApplicationRunner.java
+++ src/main/java/com/springframework/learnspringframework2/GamingAppLaunchApplicationRunner.java
@@ -3,9 +3,7 @@ package com.springframework.learnspringframework2;
 import com.springframework.learnspringframework.javagamingbasic.GameRunner;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ComponentScan("com.springframework.learnspringframework.javagamingbasic")
 public class GamingAppLaunchApplicationRunner {
     public static void main(String[] args) {
